d14d5221ef1b8e5ee226645dc70bc8d08d89a3bb
andrew speer <nottolivc@gmail.com>
2021-02-16 04:54:44 -0800

    app skeleton with basic functionality, no proxy server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const app = express();
 const fetch = require('node-fetch');
 const https = require('https');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const request = require('request')
+const request = require('request');
+const httpProxy = require('express-http-proxy')
 
 // Allow CORS
 app.all('/*', function(req, res, next) {
@@ -14,16 +15,19 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
+
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the Quake API</h1>');
 });
 
-const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
-
 app.get('/quakes', async (req, res) => {
-  //console.log(req.params)
-  console.log(req.route)
   console.log(req.query)
+  const magnitude = req.query.magnitude
+  const longitude = req.query.longitude
+  const latitude = req.query.latitude
+  const radius = req.query.radius
+  const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson`
+
   await axios.get(url)
     .then(response => {
         console.log(response.data.status);
@@ -35,6 +39,23 @@ app.get('/quakes', async (req, res) => {
     });
 });
 
+// const resp = axios({
+//   method: "GET",
+//   url: `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson`,
+//   params: {
+//     magnitude: 1,
+//     longitude: 100,
+//     latitude: 37,
+//     radius: 200,
+//     starttime: '2008-01-01',
+//     endtime: '2020-01-02',
+//     location: 'China',
+//   },
+// });
+
+// Retrieve just the data from the response
+// const { data } = resp;
+// console.log(resp);
 // request.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson', (error, response, body) => {
 //   if (error) {
 //     return console.log(error)
@@ -43,7 +64,7 @@ app.get('/quakes', async (req, res) => {
 //   //console.log(response)
 // })
 
-//app.use('/api', createProxyMiddleware({ target: 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson', changeOrigin: true }));
+app.use('/api', createProxyMiddleware({ target: 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson', changeOrigin: true }));
 
 app.listen(PORT, () => {
  console.log(`Server is listening on port: ${PORT}`);
--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-//import useCustomForm from ''./useCustomForm';
-
 
 const App = () => {
 
@@ -10,15 +8,13 @@ const App = () => {
   const [location, setLocation] = useState('');
   const [latitude, setLatitude] = useState(37);
   const [longitude, setLongitude] = useState(100);
-  const [start, setStartDate] = useState('2020-01-02');
-  const [end, setEndDate] = useState('2008-01-01');
+  const [start, setStartDate] = useState('2008-01-02');
+  const [end, setEndDate] = useState('2020-01-01');
   const [radius, setRadius] = useState(200);
   const [magnitude, setMagnitude] = useState(5);
   const [quakesData, setData] = useState([]);
-  
-  const [filtered, setFiltered] = useState([]);
-  //const [formData, setFormData] = useState({});
-  const [currentPage, setCurrentPage] = useState(0)
+  //const [currentPage, setCurrentPage] = useState(0);
+  //const [loaded, isLoading] = useState(false);
 
   useEffect(() => {    
     axios.get(`http://localhost:4000/quakes`)
@@ -26,100 +22,95 @@ const App = () => {
           setQuakes(res.data.features);
           //quakes = res.data.features;
           console.log(res.data.features);
+          //isLoading(true)
         })
         .catch(err => {
           console.log(err)
         });
-        setCurrentPage(currentPage);
+        //setCurrentPage(currentPage);
   }, [])
-
-    // useEffect(() => {
-    //     const results = filtered.filter(res => res.name.toLowerCase().includes(result)
-    //         ); 
-    //     setData(results)
-    //     }, [result])
-    //     console.log(data)
 
   const onChangeLoc = e => {
       setLocation(e.target.value);
-      //console.log(e.target.value);
   }
 
   const onChangeStart = e => {
       setStartDate(e.target.value);
-      //console.log(e.target.value);
   }
 
   const onChangeEnd = e => {
       setEndDate(e.target.value);
-      //console.log(e.target.value);
   }
- ;
+ 
   const onChangeRadius = e => {
       setRadius(e.target.value);
-      //console.log(e.target.value);
   }
 
   const onChangeLat = e => {
       setLatitude(e.target.value);
-      //console.log(e.target.value)
   }
 
   const onChangeLong = e => {
     e.preventDefault()
       setLongitude(e.target.value);
-      //console.log(e.target.value)
   }
 
   const onChangeMag = e => {
       setMagnitude(e.target.value);
-      //console.log(e)
 }
- 
-  const fetchData = () => {
-    try {
-      const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
-      const res = axios.get(`${url}&starttime=${start}&endtime=${end}&minmagnitude=${magnitude}&minmagnitude=${5}&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${radius}`);       
-      setData(res.data.features);
-      console.table(res.data.features);
-      setFiltered(res.data);
-      console.log(res.data);
-      console.log(res.data.features);
-    } catch (err) {
-        throw new Error(err);
-      }
-    };
+  let earthquakes = []
+
 
   const PER_PAGE = 100;
   const offset = PER_PAGE;
-  const pageCount = Math.ceil(quakes.length / PER_PAGE);
+  // const pageCount = Math.ceil(quakes.length / PER_PAGE);
   
   const handleSubmit = async (e) => {
     e.preventDefault()
     const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
-    const result = await axios.get(`${url}&starttime=2000-01-01&endtime=2020-01-02&minmagnitude=${magnitude}&minmagnitude=${5}&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${radius}`);       
+    const result = await axios.get(`${url}&starttime=${start}&endtime=${end}&minmagnitude=${magnitude}&minmagnitude=${5}&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${radius}`);       
     setData(result.data.features);
     console.log(result)
     console.log(result.data)
+    earthquakes.push(result.data.features)
+    console.log(earthquakes)
     console.log(magnitude);
   }
 
+
+  const getTime = () => {
+    var now = new Date();
+    return ((now.getMonth() + 1) + '-' +
+            (now.getDate()) + '-' +
+             now.getFullYear() + " " +
+             now.getHours() + '-' +
+             ((now.getMinutes() < 10)
+                 ? ("0" + now.getMinutes())
+                 : (now.getMinutes())) + ':' +
+             ((now.getSeconds() < 10)
+                 ? ("0" + now.getSeconds())
+                 : (now.getSeconds())));
+  }
+  
+  //console.log(getTime(1572987142290));
+  
   return (
     <>
     <div className="App">
+    <div className="container">
     <h1>Quakes</h1>
     <form onSubmit={handleSubmit}>
-    <p>Location</p>
+    <p>Location (Closest Country will Show)</p>
     <input type="text" onChange={onChangeLoc} value={location} />
     <p>Start date</p>
     <input type="date" onChange={onChangeStart} value={start} />
     <p>End date</p>
     <input type="date" onChange={onChangeEnd} value={end} />
-    <p>Min magnitude</p>
+    <p>Min magnitude (0-10)</p>
     <input type="number" onChange={onChangeMag} value={magnitude} />
-    <p>Latitude</p>
+    <p>Latitude (-90, 90)</p>
     <input type="number" onChange={onChangeLat} value={latitude} />
-    <p>Longitude</p>
+    <p>Longitude (-180, 180)</p>
     <input type="number" onChange={onChangeLong} value={longitude} />
     <br />
     <p>Radius (km)</p>
@@ -129,21 +120,83 @@ const App = () => {
     <button type="submit">Submit Query</button>
     </form>
     <br />
-    {quakesData.map((s, item) => {
-      //console.table(s.properties.place, s.properties.mag, item);
-      return (
+    </div>
+    <div className="container2">
+    <p>Displaying top 100 of {quakes.length}</p>
+    <p>Min Magnitude: {magnitude}</p>
+    <p>Max Magnitude: </p>
+    <p>Total Number of Earthquakes: {quakes.length}</p>
+    <br />
+    {/* {
+      //const minMags = Math.min(...mags)
+      //const maxMags = Math.max(...mags)
+      // const min = mags.reduce((a, b) => Math.min(a, b))
+      // console.log(min)
+      // const max = mags.reduce((a, b) => Math.max(a, b))
+      // console.log(max)
+      // const median = (mags) => {
+      //   if (mags.length === 0) return 0;
+      //     mags.sort(function(a,b){
+      //       return a-b;
+      //   });
+      //   var half = Math.floor(mags.length / 2);
+      //   if (mags.length % 2)
+      //     return mags[half];
+      //   return (mags[half - 1] + mags[half]) / 2.0;
+      // }
+      // console.log(median(mags))
+      // console.log(mags.pop())
         <>
-        <div key={s.id}>
-          <h2>{s.properties.title}</h2>
-          <p>Place {s.properties.place}</p>
-          <p>Magnitude {s.properties.mag}</p>
-          <p>Time {s.properties.time}</p>
-          <h5>Coordinates: </h5>
-          <h5>{s.geometry.coordinates.map((item) => { return item })}</h5>
-        </div>
-        </>);
-        })}
-      {quakes.slice(offset, offset + PER_PAGE).map((s, item) => {
+        <p>Max Magnitude{quakesData.map((s, item) => {
+          let mags = []
+          mags.push(s.properties.mag)}</p
+        </>
+      )
+      })} */}
+       <>
+        <h4>Query Results Data Table</h4>
+        {quakesData.map((s, item) => {
+            return (
+                <div key={item}>
+                <div className="table__wrap">
+                <table className="table">
+                    <thead className="table__header">
+                    <tr class="table__row">
+                        <th className="table__cell u-text-left">Title and Location</th>
+                        <th className="table__cell u-text-right">Magnitude</th>
+                        <th className="table__cell u-text-right">Time</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr className="table__row">
+                    <td className="table__account table__cell">
+                        <span className="table__account-number">{s.properties.title}</span>
+                    </td>
+                    <td className="table__balance table__cell u-text-right u-font-mono">{s.properties.mag}</td>
+                    <td className="table__limit table__cell u-text-right u-font-mono">{getTime(s.properties.time)}</td>
+                    </tr>
+                    <tr className="table__row">
+                        <td className="table__account table__cell">
+                        <span className="table__account-name">{s.properties.place}</span>
+                        </td>
+                        <td className="table__balance table__cell u-text-right u-font-mono"></td>
+                        <span className="table__account-name">Coordinates</span>
+                        <span>{s.geometry.coordinates.map((s, item) => (
+                        <div key={item}>
+                          <td className="table__limit table__cell u-text-right u-font-mono">{s}</td>
+                        </div>
+                         ))}
+                        </span>
+                    </tr>
+                    </tbody>
+                    </table>
+                    </div>
+                    </div>
+                    );
+                })}
+        </>
+        <h1>Top 100 Earthquakes List</h1>
+        {quakes.slice(offset, offset + PER_PAGE).map((s, item) => {
         return (
           <>
           <div key={s.id}>
@@ -155,8 +208,8 @@ const App = () => {
           <h5>{s.geometry.coordinates.map((item) => { return item })}</h5>
         </div>
         </>);
-         <p>Displaying top 100 of {quakes.length}</p>
         })}
+    </div>
     </div>
     </>
   );
